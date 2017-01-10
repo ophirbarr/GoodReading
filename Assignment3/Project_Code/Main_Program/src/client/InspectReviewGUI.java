f81f9f3726559cd0006dbdb9056e6e103a413319
avihaizaf <Avihaizaf@gmail.com>
2017-01-10 23:51:42 +0200

    BookReview#3

--- Assignment3/Project_Code/Main_Program/src/client/InspectReviewGUI.java
+++ Assignment3/Project_Code/Main_Program/src/client/InspectReviewGUI.java
@@ -4,53 +4,82 @@ import java.io.IOException;
 
 import javax.swing.JPanel;
 import common.Message;
+import good_reading.BookReview;
+
 import javax.swing.JToolBar;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.JList;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.DefaultListModel;
 import javax.swing.JComboBox;
 import java.awt.Button;
 import java.awt.ScrollPane;
-public class InspectReviewGUI extends JPanel {
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.AbstractListModel;
+
+
+public class InspectReviewGUI extends JPanel  {
 	
 	private ClientInterface clientInterface;
+	private BookReview[] br;
 	
-	public InspectReviewGUI(ClientInterface clientInterface) throws IOException{
+	
+	public InspectReviewGUI(ClientInterface clientInterface) {
 		super();
 		this.clientInterface = clientInterface;
 		setLayout(null);
 		
-		JLabel lblTitleBookname = new JLabel("Title                             BookName   ");
-		lblTitleBookname.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
-		lblTitleBookname.setBounds(126, 32, 277, 14);
-		add(lblTitleBookname);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(106, 57, 277, 212);
+		add(scrollPane);
 		
 		JList list = new JList();
-		list.setBounds(104, 57, 299, 221);
-		add(list);
+		list.addListSelectionListener(new ListSelectionListener() { 
+			public void valueChanged(ListSelectionEvent e) {
+				int index = list.getSelectedIndex();
+				if(index == 1)
+					System.out.println("row number 1 selected");
+				else if(index==2)System.out.println("row number 2 selected");
+				
+			}
+		});
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"Review 1", "Review 2 "};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		scrollPane.setViewportView(list);
 		
-		Button button = new Button("Open");
-		button.setFont(new Font("Dialog", Font.ITALIC, 12));
-		button.setBounds(212, 259, 69, 19);
-		add(button);
-		Message msg = new Message("getInspectReview", "BookInspectReview");
+		JLabel lblTheReviewsThat = new JLabel("The Reviews that wait to approve:");
+		lblTheReviewsThat.setFont(new Font("Tahoma", Font.ITALIC, 15));
+		lblTheReviewsThat.setBounds(102, 11, 295, 26);
+		add(lblTheReviewsThat);
+		InspectReview();
+	
+	
+}
+public void InspectReview(){
 		
+	Message msg = new Message("getInspectReview", "BookInspectReview");
+		clientInterface.msgFromServer = null;
 		try {
 			clientInterface.client.openConnection();
 			clientInterface.client.sendToServer(msg);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		try {
-			clientInterface.client.wait();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		clientInterface.waitForServer();
 		
-		
-	}
+}
 }
