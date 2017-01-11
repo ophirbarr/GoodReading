b1c7c316f3a378b92930bd0a6348dfcae155dc41
yair9786 <yair9786@gmail.com>
2017-01-11 21:43:07 +0200

    11/1/17#2

--- Assignment3/Project_Code/Main_Program/src/client/ViewBookGUI.java
+++ Assignment3/Project_Code/Main_Program/src/client/ViewBookGUI.java
@@ -25,7 +25,7 @@ public class ViewBookGUI extends JPanel {
 	 */
 	public ViewBookGUI(ClientInterface clientInterface, Book book) {
 		super();
-		setBounds(new Rectangle(0, 0, 727, 588));
+		setBounds(new Rectangle(10, -11, 727, 588));
 		setBackground(Color.WHITE);
 		this.clientInterface = clientInterface;
 		setLayout(null);
@@ -39,65 +39,65 @@ public class ViewBookGUI extends JPanel {
 		textField = new JLabel(book.get_title());
 		textField.setHorizontalAlignment(SwingConstants.LEFT);
 		textField.setFont(new Font("Tahoma", Font.BOLD, 19));
-		textField.setBounds(66, 89, 226, 51);
+		textField.setBounds(42, 41, 226, 51);
 		imagePanel.add(textField);
 		
 		JLabel lblBookpicture = new JLabel("book_picture");
 		lblBookpicture.setIcon(new ImageIcon(ViewBookGUI.class.getResource("/design/harry_potter2.png")));
-		lblBookpicture.setBounds(469, 149, 184, 270);
+		lblBookpicture.setBounds(445, 101, 184, 270);
 		imagePanel.add(lblBookpicture);
 		
 		JLabel lblIdLabel = new JLabel("ID: "+book.get_bid());
 		lblIdLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblIdLabel.setBounds(66, 151, 58, 20);
+		lblIdLabel.setBounds(42, 103, 58, 20);
 		imagePanel.add(lblIdLabel);
 		
 		JLabel lblLanguage = new JLabel("Language: "+book.get_language());
 		lblLanguage.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblLanguage.setBounds(66, 182, 184, 20);
+		lblLanguage.setBounds(42, 134, 184, 20);
 		imagePanel.add(lblLanguage);
 		
 		JLabel lblPrice = new JLabel("Price: "+book.get_price()+" $");
 		lblPrice.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPrice.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblPrice.setBounds(66, 402, 135, 38);
+		lblPrice.setBounds(42, 354, 135, 38);
 		imagePanel.add(lblPrice);
 		
 		JButton btnBuyNow = new JButton("Buy Now");
 		btnBuyNow.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btnBuyNow.setBounds(78, 451, 112, 29);
+		btnBuyNow.setBounds(54, 403, 112, 29);
 		imagePanel.add(btnBuyNow);
 		
 		JLabel lblSummary = new JLabel("Summary: ");
 		lblSummary.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblSummary.setBounds(66, 213, 184, 20);
+		lblSummary.setBounds(42, 165, 184, 20);
 		imagePanel.add(lblSummary);
 		
 		JTextArea textArea = new JTextArea(book.get_summary());
 		textArea.setFont(new Font("Courier New", Font.PLAIN, 14));
 		textArea.setEditable(false);
-		textArea.setBounds(66, 244, 346, 38);
+		textArea.setBounds(42, 196, 346, 38);
 		imagePanel.add(textArea);
 		
 		JLabel lblTableOfContents = new JLabel("Table Of Contents: ");
 		lblTableOfContents.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblTableOfContents.setBounds(66, 293, 184, 20);
+		lblTableOfContents.setBounds(42, 245, 184, 20);
 		imagePanel.add(lblTableOfContents);
 		
 		JTextArea textArea_1 = new JTextArea(book.get_TableOfContents());
 		textArea_1.setFont(new Font("Courier New", Font.PLAIN, 14));
 		textArea_1.setEditable(false);
-		textArea_1.setBounds(66, 324, 346, 38);
+		textArea_1.setBounds(42, 276, 346, 38);
 		imagePanel.add(textArea_1);
 		
 		JButton btnReadReviews = new JButton("Read Reviews");
 		btnReadReviews.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnReadReviews.setBounds(421, 474, 135, 29);
+		btnReadReviews.setBounds(397, 426, 135, 29);
 		imagePanel.add(btnReadReviews);
 		
 		JButton btnSubmitReview = new JButton("Submit Review");
 		btnSubmitReview.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnSubmitReview.setBounds(569, 474, 135, 29);
+		btnSubmitReview.setBounds(545, 426, 135, 29);
 		imagePanel.add(btnSubmitReview);
 	}
 }
