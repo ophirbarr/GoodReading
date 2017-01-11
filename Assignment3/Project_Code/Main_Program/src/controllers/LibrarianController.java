7eeb4f364f38f05fa47cec0a1305c727a835c192
innailyaev <inna_il@hotmail.com>
2017-01-11 11:55:03 +0200

    11/01/17#1inna

---
+++ Assignment3/Project_Code/Main_Program/src/controllers/LibrarianController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+public class LibrarianController {
+	
+		
+public static void AddNewUser(){}
+	
+public static void FromUserToCostumer(){}
+
+public static void EditCostumerAccount(){}
+
+public static void EraseUser(){}
+
+
+}
+
+
