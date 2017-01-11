d87f41ec98e5dd21647eeb0a80078c4f6b1c506f
yair9786 <yair9786@gmail.com>
2017-01-11 11:34:42 +0200

    11/1/17#1

--- Assignment3/Project_Code/Main_Program/src/client/ViewBookGUI.java
+++ Assignment3/Project_Code/Main_Program/src/client/ViewBookGUI.java
@@ -12,6 +12,8 @@ import javax.swing.SwingConstants;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 
 public class ViewBookGUI extends JPanel {
 
@@ -27,10 +29,6 @@ public class ViewBookGUI extends JPanel {
 		setBackground(Color.WHITE);
 		this.clientInterface = clientInterface;
 		setLayout(null);
-	
-		
-		/////////////////////////////////
-		
 		
 		JPanel imagePanel = new JPanel();
 		imagePanel.setBounds(0, 0, 727, 588);
@@ -39,9 +37,9 @@ public class ViewBookGUI extends JPanel {
 		imagePanel.setLayout(null);
 		
 		textField = new JLabel(book.get_title());
-		textField.setHorizontalAlignment(SwingConstants.CENTER);
+		textField.setHorizontalAlignment(SwingConstants.LEFT);
 		textField.setFont(new Font("Tahoma", Font.BOLD, 19));
-		textField.setBounds(186, 48, 280, 51);
+		textField.setBounds(66, 89, 226, 51);
 		imagePanel.add(textField);
 		
 		JLabel lblBookpicture = new JLabel("book_picture");
@@ -62,12 +60,44 @@ public class ViewBookGUI extends JPanel {
 		JLabel lblPrice = new JLabel("Price: "+book.get_price()+" $");
 		lblPrice.setHorizontalAlignment(SwingConstants.CENTER);
 		lblPrice.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblPrice.setBounds(66, 304, 135, 38);
+		lblPrice.setBounds(66, 402, 135, 38);
 		imagePanel.add(lblPrice);
 		
 		JButton btnBuyNow = new JButton("Buy Now");
 		btnBuyNow.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btnBuyNow.setBounds(76, 353, 112, 29);
+		btnBuyNow.setBounds(78, 451, 112, 29);
 		imagePanel.add(btnBuyNow);
+		
+		JLabel lblSummary = new JLabel("Summary: ");
+		lblSummary.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblSummary.setBounds(66, 213, 184, 20);
+		imagePanel.add(lblSummary);
+		
+		JTextArea textArea = new JTextArea(book.get_summary());
+		textArea.setFont(new Font("Courier New", Font.PLAIN, 14));
+		textArea.setEditable(false);
+		textArea.setBounds(66, 244, 346, 38);
+		imagePanel.add(textArea);
+		
+		JLabel lblTableOfContents = new JLabel("Table Of Contents: ");
+		lblTableOfContents.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblTableOfContents.setBounds(66, 293, 184, 20);
+		imagePanel.add(lblTableOfContents);
+		
+		JTextArea textArea_1 = new JTextArea(book.get_TableOfContents());
+		textArea_1.setFont(new Font("Courier New", Font.PLAIN, 14));
+		textArea_1.setEditable(false);
+		textArea_1.setBounds(66, 324, 346, 38);
+		imagePanel.add(textArea_1);
+		
+		JButton btnReadReviews = new JButton("Read Reviews");
+		btnReadReviews.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnReadReviews.setBounds(421, 474, 135, 29);
+		imagePanel.add(btnReadReviews);
+		
+		JButton btnSubmitReview = new JButton("Submit Review");
+		btnSubmitReview.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnSubmitReview.setBounds(569, 474, 135, 29);
+		imagePanel.add(btnSubmitReview);
 	}
 }
---
+++ Assignment3/Project_Code/Main_Program/src/controllers/CustomerController.java
@@ -0,0 +1,35 @@
+package controllers;
+
+public class CustomerController {
+	
+	public static void SubmitReview()
+	{
+		
+	}
+	
+	public static void DownloadBook()
+	{
+		
+	}
+	
+	public static void BuyBook()
+	{
+		
+	}
+	
+	public static void MyBooks()
+	{
+		
+	}
+	
+	public static void UpdateAccountType()
+	{
+		
+	}
+	
+	public static void ValidateAccount()
+	{
+		
+	}
+	
+}
