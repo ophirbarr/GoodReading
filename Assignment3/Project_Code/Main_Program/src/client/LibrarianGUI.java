0839b7854e41af0b979943c0ed6632eaaa33111f
ophirbarr <ophirbarr@gmail.com>
2017-01-13 17:33:12 +0200

    13/1/17 #2

--- Assignment3/Project_Code/Main_Program/src/client/LibrarianGUI.java
+++ Assignment3/Project_Code/Main_Program/src/client/LibrarianGUI.java
@@ -11,12 +11,14 @@ import good_reading.Worker;
 
 import java.awt.Font;
 import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class LibrarianGUI extends JPanel
 {
 	private ClientInterface clientInterface;
 	private JPanel menuPanel;
-	private JPanel firstPanel;
+	private JPanel currentPanel;
 
 	
 	public LibrarianGUI(ClientInterface clientInterface)
@@ -32,15 +34,54 @@ public class LibrarianGUI extends JPanel
 		menuPanel.setLayout(null);
 		
 		JButton btnNewButton = new JButton("Approve new user");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				remove(currentPanel);
+				currentPanel = new JPanel();  // TODO replace with your class
+				currentPanel.setBounds(176, 1, 724, 475);
+				currentPanel.setBackground(new Color(250, 243, 232));
+				add(currentPanel);
+				currentPanel.setLayout(null);
+				revalidate();
+				repaint();
+			}
+		});
 		btnNewButton.setBounds(21, 11, 150, 23);
 		menuPanel.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Setup new account");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				remove(currentPanel);
+				currentPanel = new JPanel();  // TODO replace with your class
+				currentPanel.setBounds(176, 1, 724, 475);
+				currentPanel.setBackground(new Color(250, 243, 232));
+				add(currentPanel);
+				currentPanel.setLayout(null);
+				revalidate();
+				repaint();
+			}
+		});
 		btnNewButton_1.setBounds(21, 42, 150, 23);
 		menuPanel.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Edit existing account");
-		btnNewButton_2.setBounds(181, 42, 150, 23);
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				remove(currentPanel);
+				currentPanel = new JPanel();  // TODO replace with your class
+				currentPanel.setBounds(176, 1, 724, 475);
+				currentPanel.setBackground(new Color(250, 243, 232));
+				add(currentPanel);
+				currentPanel.setLayout(null);
+				revalidate();
+				repaint();
+			}
+		});
+		btnNewButton_2.setBounds(181, 42, 156, 23);
 		menuPanel.add(btnNewButton_2);
 		
 		if (clientInterface.user instanceof Worker)
@@ -52,24 +93,37 @@ public class LibrarianGUI extends JPanel
 		}
 		
 		JButton btnEraseExistingUser = new JButton("Erase existing user");
-		btnEraseExistingUser.setBounds(181, 11, 150, 23);
+		btnEraseExistingUser.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				remove(currentPanel);
+				currentPanel = new JPanel();  // TODO replace with your class
+				currentPanel.setBounds(176, 1, 724, 475);
+				currentPanel.setBackground(new Color(250, 243, 232));
+				add(currentPanel);
+				currentPanel.setLayout(null);
+				revalidate();
+				repaint();
+			}
+		});
+		btnEraseExistingUser.setBounds(181, 11, 156, 23);
 		menuPanel.add(btnEraseExistingUser);
 		
-		firstPanel = new JPanel();
-		firstPanel.setBackground(new Color(250, 243, 232));
-		firstPanel.setBounds(0, 76, 799, 513);
-		add(firstPanel);
-		firstPanel.setLayout(null);
+		currentPanel = new JPanel();
+		currentPanel.setBackground(new Color(250, 243, 232));
+		currentPanel.setBounds(0, 76, 799, 513);
+		add(currentPanel);
+		currentPanel.setLayout(null);
 		
 		JLabel lblChooseFromThe = new JLabel("Choose from the actions above.");
 		lblChooseFromThe.setFont(new Font("Tahoma", Font.BOLD, 13));
 		lblChooseFromThe.setIcon(new ImageIcon(LibrarianGUI.class.getResource("/com/sun/javafx/scene/control/skin/caspian/images/capslock-icon.png")));
 		lblChooseFromThe.setBounds(22, 29, 254, 29);
-		firstPanel.add(lblChooseFromThe);
+		currentPanel.add(lblChooseFromThe);
 		
 		JLabel label = new JLabel("");
 		label.setIcon(new ImageIcon(LibrarianGUI.class.getResource("/design/g10629.png")));
 		label.setBounds(32, 69, 478, 329);
-		firstPanel.add(label);
+		currentPanel.add(label);
 	}
 }
