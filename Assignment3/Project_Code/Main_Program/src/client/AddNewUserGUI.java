4232d7e92f1ecbea4c8aff42b6f068f1a48900d1
innailyaev <inna_il@hotmail.com>
2017-01-17 19:55:49 +0200

    17/01/17#2inna

--- Assignment3/Project_Code/Main_Program/src/client/AddNewUserGUI.java
+++ Assignment3/Project_Code/Main_Program/src/client/AddNewUserGUI.java
@@ -2,27 +2,37 @@ package client;
 
 import javax.swing.JPanel;
 
+import java.awt.Component;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTable;
+import javax.swing.border.EtchedBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
+import common.Define;
 import common.Message;
+import good_reading.Book;
 import good_reading.BookReview;
 import good_reading.SystemUser;
 
 import javax.swing.JScrollPane;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 
 public class AddNewUserGUI extends JPanel {
 	
 	private ClientInterface clientInterface;
 	private SystemUser[] users ;   //a list of all awaiting approval users        
-	private int size;   //number of awaiting approval users
-	private DefaultListModel model;
+	private int size;  
+	private DefaultListModel model;	
+	private final JScrollPane scrollPane = new JScrollPane();
 
 	/**
 	 * Create the panel.
@@ -37,57 +47,89 @@ public class AddNewUserGUI extends JPanel {
 		
 		super();
 		this.clientInterface = clientInterface;
-		ViewUsers();
-
-		Message msg = (Message) clientInterface.getMsgFromServer();
-		users = (SystemUser[])msg.getParameters().get(0);
-		size=users.length;
-
-		model = new DefaultListModel();
 		setLayout(null);
+		scrollPane.setBounds(22, 210, 410, 155);
+		add(scrollPane);
+
+		DefaultListModel<String> listModel = new DefaultListModel<String>();
+		
+		JLabel ResultTitle = new JLabel("Name       LastName       ID");
+		ResultTitle.setFont(new Font("Tahoma", Font.BOLD, 11));
+		scrollPane.setColumnHeaderView(ResultTitle);
+		JList<String> list = new JList<String>( listModel );
+		scrollPane.setViewportView(list);
+		list.setFont( new Font("monospaced", Font.PLAIN, 14) );
+		
+		Message msg = new Message("ViewUsersWithCondition", "LibrarianController");
+		
+		try {
+			clientInterface.client.openConnection();
+			clientInterface.client.sendToServer(msg);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		clientInterface.waitForServer();  // Waiting for approval from the server
+
+		
+		users = (SystemUser[])clientInterface.getMsgFromServer();
+		
+		size=users.length;
+		
+		if(size==0) listModel.addElement("There is no users that waiting for approval!" );
+		else
+		{
+			for(int i=0;i<size;i++)  
+			{
+			listModel.addElement(users[i].get_firstName());
+			}
+		}
+		JLabel lblTheReviewsThat = new JLabel("Users that waiting for approve:");
+		lblTheReviewsThat.setBounds(41, 95, 281, 29);
+		lblTheReviewsThat.setFont(new Font("Tahoma", Font.ITALIC, 15));
+		add(lblTheReviewsThat);
 		
 		JButton btnAddUser = new JButton("Add");
-		btnAddUser.setBounds(345, 61, 76, 42);
+		btnAddUser.setBounds(156, 137, 83, 44);
 		add(btnAddUser);
 		
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(38, 61, 277, 212);
-		add(scrollPane);
+		list.addListSelectionListener(new ListSelectionListener() {
+			
 		
-		if(size==0) model.addElement("There is no users that waiting for approval!" );
-		else for(int i=0;i<size;i++)  //additional the reviews to Jlist
-			model.addElement(String.format("%-50s%-40s", users[i].get_firstName()));
-		
-		
-		JLabel lblWrittenForBook = new JLabel("UserName                            UserId");
-		scrollPane.setColumnHeaderView(lblWrittenForBook);
-		JList list = new JList();
-		scrollPane.setViewportView(list);
-		
-		list.setModel(model);
-		
-		JLabel lblTheReviewsThat = new JLabel("The Users that waiting for approve:");
-		lblTheReviewsThat.setFont(new Font("Tahoma", Font.ITALIC, 15));
-		lblTheReviewsThat.setBounds(38, 11, 295, 26);
-		add(lblTheReviewsThat);
+			public void valueChanged(ListSelectionEvent e) {
+				int index = list.getSelectedIndex();
+				
+				btnAddUser.addActionListener(new ActionListener() {
+
+					public void actionPerformed(ActionEvent arg0) {
+						
+						try {
+							Message msg = new Message("AddNewUser", "LibrarianController");
+							msg.add(users[index].get_uid());
+							clientInterface.client.openConnection();
+							clientInterface.client.sendToServer(msg);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						
+						//clientInterface.waitForServer();
+						JOptionPane.showMessageDialog(null, "The User Status Changed To DISCONNECTED");
+					}
 					
-		}
-	
-	
-	public void ViewUsers() {
-		
-		Message msg = new Message("ViewUsersWithCondition", "LibrarianController");
-			clientInterface.msgFromServer = null;
-			try {
-				clientInterface.client.openConnection();
-				clientInterface.client.sendToServer(msg);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			clientInterface.waitForServer();  // Waiting for approval from the server
+				});
+				
+				
+			} 	
+			
+		});
 			
 	}
-}
+}		
+					
+		
+	
+
+
 
 	
