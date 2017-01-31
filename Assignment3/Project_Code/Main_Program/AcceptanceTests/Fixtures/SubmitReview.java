160dff6b6343475ee1f00de383e788ff0650774a
yair9786 <yair9786@gmail.com>
2017-02-01 00:51:33 +0200

    1/2/17#2

--- Assignment3/Project_Code/Main_Program/AcceptanceTests/Fixtures/SubmitReview.java
+++ Assignment3/Project_Code/Main_Program/AcceptanceTests/Fixtures/SubmitReview.java
@@ -1,10 +1,23 @@
 package Fixtures;
 
 import fit.ActionFixture;
+import good_reading.Book;
+import good_reading.BookReview;
 
 
 public class SubmitReview extends ActionFixture{
 
-	
+	public void Initialize()
+	{
+		Book book = new Book();
+		
+		book.set_title("book title");
+		BookReview review = new BookReview();
+		
+		review.set_bid(book.get_bid());
+		review.set_review("review text");
+		review.set_costumerName("");
+		review.set_approved(false);
+	}
 	
 }
