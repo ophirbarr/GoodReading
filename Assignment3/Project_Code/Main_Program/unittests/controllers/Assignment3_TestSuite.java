9c76337c6b0145ffb4918225ba12f1563d79cfe3
ophirbarr <ophirbarr@gmail.com>
2017-02-04 11:04:42 +0200

    4/2/17 #1 test suite created

---
+++ Assignment3/Project_Code/Main_Program/unittests/controllers/Assignment3_TestSuite.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ RemoveBookTest.class, SearchBooksTest.class })
+public class Assignment3_TestSuite {
+
+}
